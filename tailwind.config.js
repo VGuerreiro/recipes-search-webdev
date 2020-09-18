df7be73f0a0b16aa29a0c8d28b6731a09e0bc3de
VGuerreiro <vepguerreiro@gmail.com>
2020-09-18 18:16:55 +0100

    project start commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      // colors: {
+      //   primary: '#FF6363',
+      //   secondary: {
+      //     100:'#E2E2D5',
+      //     200: '#8888883',
+      //   }
+      // }
+      fontFamily:{
+        body: ['Montserrat'],
+        title: ['Pacifico']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
